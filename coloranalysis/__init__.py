087667d96ba66c7a5432d29982be9d68e97ac33a
sravyadhulipala <sravyadhulipala@github.com>
2020-02-24 10:44:43 +0530

    import required libs

--- coloranalysis/__init__.py
+++
@@ -1,6 +0,0 @@
-import .cv2 as cv
-import .numpy as np
-import .matplotlib.pyplot as plt
-from .mpl_toolkits.mplot3d import Axes3D
-from .matplotlib import colors
-from .matplotlib.colors import rgb_to_hsv
--- coloranalysis/coloranalysis.py
+++ coloranalysis/coloranalysis.py
@@ -1,3 +1,10 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import colors
+from matplotlib.colors import rgb_to_hsv
+
 class colorAnalysis:
     
     #Conversion of 1. HEX code to RGB and 2. RGB to HSV
